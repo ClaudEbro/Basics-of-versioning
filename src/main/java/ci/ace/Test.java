a82a4221dfda77c8805f3c2bb310175a6982ad10
ClaudEbro <claude.ebrottie@gmail.com>
2023-03-23 14:50:58 +0000

    Comments

--- src/main/java/ci/ace/Test.java
+++ src/main/java/ci/ace/Test.java
@@ -1,7 +1,11 @@
+/**
+ Comment
+ */
 package main.java.ci.ace;
 
 public class Test{
     public static void main(String[]args){
+        //------Other comment
         System.out.println("Hello !");
     }
 }
